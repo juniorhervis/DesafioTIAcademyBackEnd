4b5c68e3a7f07333f71174339b3b55b302342cf4
Edson Luiz Hervis Junior <juniorhervis1@gmail.com>
2021-12-14 16:06:00 -0300

    Adicionado mais rotas

--- Controller.js
+++ Controller.js
@@ -5,6 +5,7 @@ const models = require("./models");
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 
 let cliente = models.Cliente;
 let itemPedido = models.ItemPedido;
@@ -14,57 +15,168 @@ let servico = models.Servico;
 app.get("/", function (req, res) {
   res.send("Deu boa !");
 });
-
-app.get("/servicos", async (req, res) => {
-  await servico.create({
-    nome: "Nodejs",
-    descricao: "Desenvolvimento de aplicação back-end",
-    createAt: new Date(),
-    updateAt: new Date(),
-  });
-  res.send("Serviço node criado com sucesso!");
-});
-
-app.get("/clientes", async (req, res) => {
-  await cliente.create({
-    nome: "Pedro",
-    endereco: "Rua das Flores",
-    cidade: "Curitiba",
-    uf: "Paraná",
-    nascimento: 12 / 03 / 2000,
-    clienteDesde: 15 / 07 / 2019,
-    createAt: new Date(),
-    updateAt: new Date(),
-  });
-  res.send("Registro de Cliente criado com sucesso!");
-});
-
-app.get("/pedidos", async (req, res) => {
-  await pedido.create({
-    dataPedido: 13 / 12 / 2021,
-    createAt: new Date(),
-    ClienteId: 2,
-    updateAt: new Date(),
-    
-  });
-  res.send("Pedido criado com sucesso!");
-});
-
-app.get("/item", async (req, res) => {
-    await itemPedido.create({
-      PedidoId: 1,
-      ServicoId: 1,
-      quantidade: 12,
-      valor: 12.50,
-      createAt: new Date(),
-      updateAt: new Date(),
-      
+//
+//
+//
+app.post("/servicos", async (req, res) => {
+  await servico
+    .create(req.body)
+    .then(function () {
+      return res.json({
+        erro: false,
+        message: "Serviço criado com sucesso!",
+      });
+    })
+    .catch(function (erro) {
+      return res.status(400).json({
+        error: true,
+        message: "Foi impossível se conectar.",
+      });
     });
-    res.send("Item pedido criado com sucesso!");
+});
+//
+//
+//
+app.post("/clientes", async (req, res) => {
+  await cliente
+    .create(req.body)
+    .then(function () {
+      return res.json({
+        erro: false,
+        message: "Cliente criado com sucesso!",
+      });
+    })
+    .catch(function (erro) {
+      return res.status(400).json({
+        error: true,
+        message: "Foi impossível se conectar.",
+      });
+    });
+});
+//
+//
+//
+app.post("/pedidos", async (req, res) => {
+  await pedido
+    .create(req.body)
+    .then(function () {
+      return res.json({
+        erro: false,
+        message: "Pedido criado com sucesso!",
+      });
+    })
+    .catch(function (erro) {
+      return res.status(400).json({
+        error: true,
+        message: "Foi impossível se conectar.",
+      });
+    });
+});
+//
+//
+//
+app.post("/item", async (req, res) => {
+  await itemPedido
+    .create(req.body)
+    .then(function () {
+      return res.json({
+        erro: false,
+        message: "item criado com sucesso!",
+      });
+    })
+    .catch(function (erro) {
+      return res.status(400).json({
+        error: true,
+        message: "Foi impossível se conectar.",
+      });
+    });
+});
+
+app.get("/listaservicos", async (req, res) => {
+  await servico
+    .findAll({
+      order: [["nome", "DESC"]],
+    })
+    .then(function (servicos) {
+      return res.json({ servicos });
+    })
+    .catch(function (erro) {
+      return res.status(400).json({
+        error: true,
+        message: "Foi impossível se conectar.",
+      });
+    });
+});
+
+app.get("/listaclientes", async (req, res) => {
+  await cliente
+    .findAll({
+      order: [["Id", "ASC"]],
+    })
+    .then(function (clientes) {
+      return res.json({ clientes });
+    })
+    .catch(function (erro) {
+      return res.status(400).json({
+        error: true,
+        message: "Foi impossível se conectar.",
+      });
+    });
+});
+
+app.get("/listapedidos", async (req, res) => {
+  await pedido
+    .findAll({
+      raw: true,
+    })
+    .then(function (pedidos) {
+      return res.json({ pedidos });
+    })
+    .catch(function (erro) {
+      return res.status(400).json({
+        error: true,
+        message: "Foi impossível se conectar.",
+      });
+    });
+});
+
+app.get("/ofertaservicos", async (req, res) => {
+  await servico.count("id").then(function (servicos) {
+    res.json({ servicos });
   });
+});
+
+app.get("/contarclientes", async (req, res) => {
+  await cliente.count("id").then(function (clientes) {
+    res.json({ clientes });
+  });
+});
+
+app.get("/contarpedidos", async (req, res) => {
+  await pedido.count("id").then(function (pedidos) {
+    res.json({ pedidos });
+  });
+});
+
+app.get("/servico/:id", async (req, res) => {
+  await servico
+    .findByPk(req.params.id)
+    .then((servID) => {
+      return res.json({
+        erro: false,
+        servID,
+      });
+    })
+    .catch(function (error) {
+      return res.status(400).json({
+        error: true,
+        message: "Não foi possível conectar!",
+      });
+    });
+});
 
 let port = process.env.PORT || 3001;
 
 app.listen(port, (req, res) => {
-  console.log("API Started: http://localhost:3001"); 
+  console.log("API Started: http://localhost:3001");
 });
